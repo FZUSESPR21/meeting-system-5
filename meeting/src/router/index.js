e17fbf8bdb60fef13233c436e7ad5f373f9179bb
qiqiqi-gn <65895305+qiqiqi-gn@users.noreply.github.com>
2021-03-27 17:04:25 +0800

    登陆页面添加背景特效

--- meeting/src/router/index.js
+++ meeting/src/router/index.js
@@ -9,6 +9,8 @@ import timeline from '@/components/client/index/timeline'
 import timeline2 from '@/components/client/index/timeline2'
 import pageHeader from '@/components/common/pageHeader'
 import card from '@/components/common/card'
+import Home from '../views/login/Home.vue'
+import Login from '../views/login/Login.vue'
 
 //views
 import index from '@/views/client/index'
@@ -82,6 +84,19 @@ export default new Router({
                 name: 'user_num_table',
                 component: UserNumTable
             }]
-        }
+        },
+        {
+            path: '/',
+            name: 'Home',
+            component: Home
+          },
+          {
+            path: '/login',
+            name: 'Login',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: Login
+          }
     ]
 })
