63034c373f9f53bed0568d70fae2935f63361232
zhengkaixin <1321760227@qq.com>
2021-03-27 20:31:52 +0800

    修改了 获取分论坛信息函数的调用 实现 秘书获取所有用户信息

---
+++ backend/Meeting_System/src/main/java/com/team/meetingsystem/controller/AccountInfoController.java
@@ -0,0 +1,37 @@
+package com.team.meetingsystem.controller;
+
+import com.sun.corba.se.impl.protocol.giopmsgheaders.RequestMessage;
+import com.team.meetingsystem.dao.AccountMapper;
+import com.team.meetingsystem.model.ResponseMessage;
+import com.team.meetingsystem.service.AccountInfoService;
+import com.team.meetingsystem.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/secretary")
+public class AccountInfoController {
+    @Autowired
+    private AccountInfoService accountInfoService;
+
+    @GetMapping("/info")
+    public ResponseMessage all(HttpServletRequest request){
+        HttpSession session= request.getSession();
+        Object object = session.getAttribute("userName");
+        /*if(null==object){
+            return new ResponseMessage(201, "授权已过期",object);//用户授权已经过期，设置为201
+        }
+        else{
+            if(Integer.parseInt(session.getAttribute("status").toString())==2){
+                return ResponseMessage.success(accountInfoService.selectAll());
+            }
+            return new ResponseMessage(202, "用户没有权限",object);
+        }*/return ResponseMessage.success(accountInfoService.selectAll());
+    }
+
+}
--- backend/Meeting_System/src/main/java/com/team/meetingsystem/controller/ForumController.java
+++ backend/Meeting_System/src/main/java/com/team/meetingsystem/controller/ForumController.java
@@ -2,6 +2,7 @@ package com.team.meetingsystem.controller;
 
 import com.team.meetingsystem.dao.ForumMapper;
 import com.team.meetingsystem.model.ResponseMessage;
+import com.team.meetingsystem.service.ForumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/homepage")
 public class ForumController {
     @Autowired
-    private ForumMapper forumMapper;
+    private ForumService forumService;
 
     @GetMapping("/forum")
     public ResponseMessage all(){
-
-        return ResponseMessage.success(forumMapper.selectAll());
+        return ResponseMessage.success(forumService.selectAll());
     }
 
 }
