9d7abc545bc05e23a678f6ba8271f628d7d7f9be
lvvvv <375042084@qq.com>
2021-03-27 20:42:09 +0800

    config全局配置url

---
+++ meeting/vue.config.js
@@ -0,0 +1,17 @@
+// vue.config.js
+
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+module.exports = {
+    devServer: {
+        open: false,
+        // 跨域
+        proxy: {
+            '/nuojinadm/': {
+                target: 'http://49.100.89.20',
+                changeOrigin: true
+            }
+        }
+    }
+}
