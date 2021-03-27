0c7a03703f00d3ef5577cf9439b05e1224a08080
lvvvv <375042084@qq.com>
2021-03-27 16:16:35 +0800

    admin页面

--- meeting/src/router/index.js
+++ meeting/src/router/index.js
@@ -2,20 +2,31 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import test from '@/components/test'
+import Admin from '@/views/admin/Admin'
+import UserNumTable from '@/components/admin/UserNumTable'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/HelloWorld',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: test
-    }
-  ]
+    routes: [{
+            path: '/HelloWorld',
+            name: 'HelloWorld',
+            component: HelloWorld
+        },
+        {
+            path: '/test',
+            name: 'test',
+            component: test
+        },
+        {
+            path: '/admin',
+            name: 'admin',
+            component: Admin,
+            children: [{
+                path: '/user_num',
+                name: 'user_num_table',
+                component: UserNumTable
+            }]
+        }
+    ]
 })
