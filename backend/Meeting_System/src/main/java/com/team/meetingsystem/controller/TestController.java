ef982ff99002df26a49b8ecaae0b5380bdd9b489
xydf <690054845@qq.com>
2021-03-27 20:42:47 +0800

    feat: 添加数据库连接池

--- backend/Meeting_System/src/main/java/com/team/meetingsystem/controller/TestController.java
+++ backend/Meeting_System/src/main/java/com/team/meetingsystem/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.team.meetingsystem.controller;
 
-import com.team.meetingsystem.dao.AccountForumMapper;
-import com.team.meetingsystem.model.AccountForum;
+import com.team.meetingsystem.dao.ForumMapper;
 import com.team.meetingsystem.model.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
     @Autowired
-    private AccountForumMapper accountForumMapper;
+    private ForumMapper forumMapper;
 
     @GetMapping("/hello")
     public String hello() {
@@ -24,8 +23,8 @@ public class TestController {
 
     @GetMapping("/all")
     public ResponseMessage all(){
-        accountForumMapper.insert(new AccountForum("用户2", 1));
-        System.out.println(accountForumMapper.selectByName("用户2"));
-        return ResponseMessage.success("ok");
+//        accountForumMapper.insert(new AccountForum("用户2", 1));
+//        System.out.println(accountForumMapper.selectByName("用户2"));
+        return ResponseMessage.success(forumMapper.selectAll());
     }
 }
