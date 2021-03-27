c3e14d00e08ecf53181e9241ce4b4b849ddc3569
2689124501 <2689124501@qq.com>
2021-03-27 21:03:31 +0800

    feat: 登陆

--- backend/Meeting_System/src/main/java/com/team/meetingsystem/service/AccountService.java
+++ backend/Meeting_System/src/main/java/com/team/meetingsystem/service/AccountService.java
@@ -1,6 +1,8 @@
 package com.team.meetingsystem.service;
 
 import com.team.meetingsystem.model.Account;
+import com.team.meetingsystem.model.AccountForum;
+import com.team.meetingsystem.model.Notice;
 
 import java.util.List;
 
@@ -9,7 +11,18 @@ import java.util.List;
  */
 public interface AccountService {
 
-    boolean login(Account account);
+    boolean checkAccountExists(String name);        //判断用户是否存在
+    boolean checkPassword(String name, String password);             //判断密码是否正确
+    boolean checkStatus(Account account);
+    void register(Account account);
+    void insertAccountForum(AccountForum accountForum);
 
-    List<Account> getAccounts();
+
+    List<Account> getAccounts();        //返回用户列表
+
+    Account getAccountByName(String name);
+
+    List<AccountForum> getAccountForums(String name);
+
+    List<Notice> getNoticeByForumid(Integer forumid);
 }
